8e9cc34bc15b7922b817342e104296fd0ffe8aa4
Prashant Malviya <prashantmalviya272002@gmail.com>
2025-01-14 10:48:04 +0530

    code updated

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+// let a: string= "krishna";
+// let num:number=343;
+
+// let a: string;
+// let number: number;
+// let check : boolean;
+
+// let a; //when we declare like this then the type woulb be any , and it is not recommended as well "avoid using any type"
+// let number;
+// let check;
+
+// let number = <string>"32";
+
+// let surname:string | number;
+
+// surname = "421"
+
+// const func = (n:number,m:number):number=>{
+//     console.log(n,m);
+
+// return n*m
+// return String(n*m);
+//     return n*m
+// };
+
+// type UserName = string | number; //this is known as type aliases
+
+// let a: UserName=false
+
+// type UserName = (n: number, m: number) => number;
+
+// const func: UserName = (n, m) => {
+//   console.log(n, m);
+//   return n * m;
+// };
+
+
+// --------------------------------------------------
+
+const arr: number[] = [12,2,2,4,2,1];
+
+const arr2:string[] = ["Krishna"];
+
