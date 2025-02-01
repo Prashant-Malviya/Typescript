7fc2e784c641242ca08f34da46c987be59a8e602
Prashant Malviya <prashantmalviya272002@gmail.com>
2025-02-01 23:07:52 +0530

    code updated

--- ReactTs Todo/src/utils/features.ts
+++ ReactTs Todo/src/utils/features.ts
@@ -2,9 +2,8 @@ export const saveTodos = (todos: TodoItemType[]): void => {
   localStorage.setItem("mytodos", JSON.stringify(todos));
 };
 
-
 export const getTodos = (): TodoItemType[] => {
   const todos = localStorage.getItem("mytodos");
 
-  return todos?JSON.parse(todos): [];
+  return todos ? JSON.parse(todos) : [];
 };
